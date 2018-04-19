c36bb46d2de5d60dad3ac5ff761c181963ea1d1f
Patryk Szeliga <szeliga_patryk92@gmail.com>
2018-04-19 17:09:58 +0200

    Fetch 'hello world' from API

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -8,15 +8,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import './style.scss';
+import { BASE_URL, WELCOME_PATH } from "utils/apiEndpoints";
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: "initial message"
+    };
+  }
   /**
    * when initial state username is not null, submit the form to load repos
    */
   componentDidMount() {
+    this.fetchMessage()
     if (this.props.username && this.props.username.trim().length > 0) {
       this.props.onSubmitForm();
     }
+  }
+
+  fetchMessage() {
+    fetch(BASE_URL + WELCOME_PATH).then((response) => {
+      return response.json();
+    }).then((data) => {
+      this.setState({
+        message: data.message
+      })
+    }).catch((error) => {
+      this.setState({
+        message: error
+      })
+    });
   }
 
   render() {
@@ -31,6 +53,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
           <section className="centered">
             <h2>Witaj w aplikacji FFFS Breeder</h2>
             <p>aplikacji dla hodowców zwierząt</p>
+            <span>{ this.state.message }</span>
           </section>
         </div>
       </article>
